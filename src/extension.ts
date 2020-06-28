4f02b851f9e8ddce7d013d0aaa6b2a6a3099642e
Obaida <15270128+TA4life@users.noreply.github.com>
2020-06-28 00:07:30 -0400

    File reader

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,22 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as  fs from 'fs';
 import * as vscode from 'vscode';
+
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
+	//vs 1.0 using javascript build in fs
+	fs.readFile('FILE_PATH_HERE',function(err,data){
+		if(err){
+			console.log(err);
+		}
+		console.log(data);
+	});
+	//vs 2.0 using https://code.visualstudio.com/api/references/vscode-api#workspace
+	vscode.workspace.fs.readFile(vscode.Uri);
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "visualize-control-flow" is now active!');
@@ -13,6 +24,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
+
 	let disposable = vscode.commands.registerCommand('visualize-control-flow.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
